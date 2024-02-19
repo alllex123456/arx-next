aaf4d0df1fda0cd130e05f719751b472808b7269
alllex123456 <alextanase454@gmail.com>
2024-02-19 17:46:50 +0200

    --

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
 import '@/styles/globals.css';
 import { AnimatePresence } from 'framer-motion';
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Lenis from '@studio-freight/lenis';
 
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <>
       <Header />
